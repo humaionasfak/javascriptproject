d77538f01e859e437645ba7e4dbce3cdb81be316
humaionasfak <humaionasfak45@gmail.com>
2024-06-24 01:55:22 -0700

    weather app

---
+++ tudu-list app/script.js
@@ -0,0 +1,24 @@
+// let greet = "hello world";
+// console.log(greet);
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+// let i = 0;
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+// let j = 0;
+// do {
+//   console.log(j);
+//   j++;
+// } while (j <= 5);
+// function addNumber(a, b) {
+//   return a + b;
+// }
+// let sum = addNumber(5, 10);
+// console.log(sum);
+const calAreaOfRec = (width, height) => {
+  return width * height;
+};
+console.log(calAreaOfRec(5, 20));
